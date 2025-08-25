2d4e348c1a73ba167b6395bbeca072a7e8b4c978
Miriellen tavares <miriellen.tavares23@gmail.com>
2025-08-25 12:11:04 -0300

    atividade 17
    Tiffany e Miriellen

--- Atividade2/node_2d.gd
+++ Atividade2/node_2d.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+#Minha segunda atividade utilizando godot e criando uma nova pasta dentro dele.
 func _on_button_pressed() -> void:
 	print (" segunda atividade da Miriellen")
 	pass # Replace with function body.
